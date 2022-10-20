d5ed95199be40071bac7765739dee7294e0d2fca
baboolee <migakdesign@naver.com>
2022-10-20 20:34:27 +0900

    20221020

--- js/main.js
+++ js/main.js
@@ -2,5 +2,9 @@ $(function () {
     $('.top_banner_slide').slick({
         arrows: false,
         dots: true,
+    });
+
+    $('.top_banner i').on('click', function () {
+        $('.top_banner').slideUp(200)
     })
 })
