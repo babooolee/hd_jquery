abd1683c4c27e5e222e45b88550b8d5a9bb8b4ed
baboolee <migakdesign@naver.com>
2022-10-21 21:52:43 +0900

    20221021

--- js/main.js
+++ js/main.js
@@ -24,12 +24,27 @@ $(function () {
 
     $('.main_slide').slick({
         arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
     });
+
+    $('.main_slide').on('afterChange', function (e, s, c) {
+        $('.main_visual .page li').eq(c).addClass('on').siblings().removeClass('on');
+        // c= 0,1,2
+        $('.num strong').text("0" + (c + 1))
+    });
+
+    $('.page li').on('click', function () {
+        var idx = $(this).index() // 0,1,2
+        $('.main_slide').slick('slickGoTo', idx)
+    })
 
     $('.main_visual .arrows i:first-child').on('click', function () {
         $('.main_slide').slick('slickPrev')
     })
     $('.main_visual .arrows i:last-child').on('click', function () {
         $('.main_slide').slick('slickNext')
-    })
+    });
+
+
 })
